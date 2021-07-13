ec400248f652c2291aacb696929acfbdb31cafce
Grishma Sanghavi <manishasanghavi43@gmail.com>
2021-07-13 18:14:54 +0530

    add HOD and Layout

---
+++ planning.js
@@ -0,0 +1,18 @@
+//Book my show planning
+
+// => a we need a reusable carousel
+// => Hero carousel
+// => common carousel
+// => cast and crew small carousel
+// pages
+// =>home page
+// category page
+// movie page
+// auth page
+// razorpay payment gateway
+
+// play=> how are we building it???
+// layouts
+// routes
+// pages
+// components
